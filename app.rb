da7185d207464728b1b970fa153e85f9bee8e671
Marc Mills <mdmills1990@gmail.com>
2018-11-19 17:58:55 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -29,8 +29,8 @@ class App < Sinatra::Base
 
   get '/:operation/:number1/:number2' do
     @operation = params[:operation]
-    @number1 = params[:number1].to_i
-    @number2 = params[:number2].to_i
+    @number1 = params[:number1].to_float
+    @number2 = params[:number2].to_float
     if @operation == 'add'
       "#{@number1 + @number2}"
     elsif @operation == 'subtract'
