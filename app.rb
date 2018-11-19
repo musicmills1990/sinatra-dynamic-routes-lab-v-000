2dbb57c47feae037742f30a8b6b385afe94767e7
Marc Mills <mdmills1990@gmail.com>
2018-11-19 17:53:58 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -28,13 +28,11 @@ class App < Sinatra::Base
   end
 
   get '/:operation/:number1/:number2' do
-    @operation = params[:operation].to_sym
+    @operation = params[:operation]
     @number1 = params[:number1].to_i
     @number2 = params[:number2].to_i
     "#{@number1 @operation @number2}"
   end
 
-  
+
 end
-
-
