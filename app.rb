a1bab15b01f2efdaa4710c49d9c588100468f97e
Marc Mills <mdmills1990@gmail.com>
2018-11-19 17:52:43 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -26,4 +26,15 @@ class App < Sinatra::Base
     @word5 = params[:word5]
     "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
   end
+
+  get '/:operation/:number1/:number2' do
+    @operation = params[:operation].to_sym
+    @number1 = params[:number1].to_i
+    @number2 = params[:number2].to_i
+    "#{@number1 @operation @number2}"
+  end
+
+  
 end
+
+
